edbc659568c55b1aee7d5923aea2b4c1e61471ac
martinyounghoonkim <hoons0131@gmail.com>
2020-01-24 15:58:20 +0900

    INSERT Query 작성

--- src/main/java/me/whiteship/Application.java
+++ src/main/java/me/whiteship/Application.java
@@ -13,7 +13,8 @@ public class Application {
 
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("Connection created:" + connection);
-            String sql = "CREATE TABLE ACCOUNT (id int, username varchar(255), password varchar(255))";
+            // String sql = "CREATE TABLE ACCOUNT (id int, username varchar(255), password varchar(255))";
+            String sql = "INSERT INTO ACCOUNT VALUES (1, 'Martin', '1234')";
             try (PreparedStatement statement = connection.prepareStatement(sql)) {
                 statement.execute();
             }
