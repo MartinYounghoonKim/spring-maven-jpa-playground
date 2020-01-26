b3f96091e4b5cd7cb7ba1e3197c3a97d0aaf5394
martinyounghoonkim <hoons0131@gmail.com>
2020-01-27 00:47:56 +0900

    스프링 데이터 Common3. Null 처리

--- src/test/java/com/whiteship/springmavenjpaplayground/CommentRepositoryTest.java
+++ src/test/java/com/whiteship/springmavenjpaplayground/CommentRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,5 +28,12 @@ public class CommentRepositoryTest {
 
         long count = commentRepository.count();
         assertThat(count).isEqualTo(1);
+
+        Optional<Comment> byId = commentRepository.findById(100L);
+        Comment comment1 = byId.orElse(new Comment());
+        assertThat(byId).isEmpty();
+        System.out.println(comment1.toString());
+        assertThat(comment1).isNotNull();
+
     }
 }
