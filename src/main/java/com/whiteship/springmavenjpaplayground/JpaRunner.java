b0726223afeacb67ecedf28cf8e117609ed22e30
martinyounghoonkim <hoons0131@gmail.com>
2020-01-26 14:47:41 +0900

    Comment / Post 양 방향 및 Cascade

--- src/main/java/com/whiteship/springmavenjpaplayground/JpaRunner.java
+++ src/main/java/com/whiteship/springmavenjpaplayground/JpaRunner.java
@@ -3,6 +3,7 @@ package com.whiteship.springmavenjpaplayground;
 import com.whiteship.springmavenjpaplayground.entity.Member;
 import com.whiteship.springmavenjpaplayground.entity.MemberWithTeam;
 import com.whiteship.springmavenjpaplayground.entity.Team;
+import javafx.geometry.Pos;
 import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -21,45 +22,59 @@ public class JpaRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Account account = new Account();
-        account.setUsername("Martin");
-        account.setPassword("martin");
-
-        Member member = new Member();
-        member.setName("Martin");
-        member.setPeriod(null);
-        member.setAddress1(null);
-
-        Study study = new Study();
-        study.setName("Spring Data JPA");
-
-        account.addStudy(study);
-
         Session session = entityManager.unwrap(Session.class);
-        session.save(member);
-        session.save(account);
-        session.save(study);
-        System.out.println(" getOwner =============== ");
-        System.out.println(study.getOwner());
-        System.out.println(account.getStudies());
-        System.out.println(" getOwner =============== ");
+
+        Post post = new Post();
+        post.setTitle("제목1");
+        Comment comment = new Comment();
+        comment.setComment("코멘트1");
+        post.addComment(comment);
+
+        Comment comment1 = new Comment();
+        comment1.setComment("곧 보여드릴게요.");
+        post.addComment(comment1);
+
+        session.save(post);
+        /**
+         * Account 클래스와 Study 클래스 양방향 관계
+         */
+//        Account account = new Account();
+//        account.setUsername("Martin");
+//        account.setPassword("martin");
+//
+//        Study study = new Study();
+//        study.setName("Spring Data JPA");
+//
+//        account.addStudy(study);
+//
+//        session.save(account);
+//        session.save(study);
+
+        /**
+         * Member 클래스
+         */
+//        Member member = new Member();
+//        member.setName("Martin");
+//        member.setPeriod(null);
+//        member.setAddress1(null);
+//        session.save(member);
+
         /**
          * TEAM과 맴버의 단방향
          */
-
-        Team team = new Team();
-        team.setName("team1");
-
-        MemberWithTeam memberWithTeam = new MemberWithTeam();
-        memberWithTeam.setUsername("martin");
-        team.getMembers().add(memberWithTeam);
-        memberWithTeam.setTeam(team);
-
-        session.save(team);
-        session.save(memberWithTeam);
-
-        System.out.println(" =============== ");
-        System.out.println(team.getMembers());
-        System.out.println(" =============== ");
+//        Team team = new Team();
+//        team.setName("team1");
+//
+//        MemberWithTeam memberWithTeam = new MemberWithTeam();
+//        memberWithTeam.setUsername("martin");
+//        team.getMembers().add(memberWithTeam);
+//        memberWithTeam.setTeam(team);
+//
+//        session.save(team);
+//        session.save(memberWithTeam);
+//
+//        System.out.println(" =============== ");
+//        System.out.println(team.getMembers());
+//        System.out.println(" =============== ");
     }
 }
