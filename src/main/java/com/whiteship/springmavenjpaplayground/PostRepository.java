8d91f07d8209fcba6abb4784200895c5443d9d45
martinyounghoonkim <hoons0131@gmail.com>
2020-01-26 19:45:48 +0900

    Test 코드 추가

--- src/main/java/com/whiteship/springmavenjpaplayground/PostRepository.java
+++ src/main/java/com/whiteship/springmavenjpaplayground/PostRepository.java
@@ -1,7 +1,12 @@
 package com.whiteship.springmavenjpaplayground;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    // https://docs.spring.io/spring-data/jpa/docs/2.2.4.RELEASE/reference/html/#reference
+    Page<Post> findByTitleContains (String title, Pageable pageable);
 
+    long countByTitleContains(String title);
 }
